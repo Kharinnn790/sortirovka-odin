66ceb843873c559726c4e4c43f4eaa1385af84fc
Ilya <iluxapix@gmail.com>
2025-02-05 23:00:24 +0300

    Add files via upload

---
+++ sortirovka_third/sortirovka_third/sortirovka_third.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+
+using namespace std;
+// функция сортировки пузырьком
+void bubbleSort(vector<int>& arr) {
+    int n = arr.size();
+    for (int i = 0; i < n - 1; ++i) {
+        for (int j = 0; j < n - i - 1; ++j) {
+            // если текущий элемент больше следующего, меняем их местами
+            if (arr[j] > arr[j + 1]) {
+                swap(arr[j], arr[j + 1]);
+            }
+        }
+    }
+}
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    // исходный массив
+    vector<int> array = { 12, 2145, 8, 4016, 4, 2, 1, 1000, 3, 10004, 10, 15, 235, 6 };
+
+    // сортируем пузырьком
+    bubbleSort(array);
+
+    // вывод отсортированного массива
+    cout << "Изначальный массив: {12, 2145, 8, 4016, 4, 2, 1, 1000, 3, 10004, 10, 15, 235, 6}" << endl;
+    cout << "" << endl;
+    cout << "Отсортированный массив: ";
+    for (int num : array) {
+        cout << num << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
+
